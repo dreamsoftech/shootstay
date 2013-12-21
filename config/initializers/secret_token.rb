5ef3e1e0488bdf04c2ea2040b0ec29ff1da01134
reedkevin88 <reedkevin88@gmail.com>
2013-12-22 01:13:58 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shootstay::Application.config.secret_token = '6bd3b87d2c3c9bff288744f8e0316c8a1cb0c619d1d07bdf15a022a2860e923b55bd178bdf416ae2040bbf2edf172e58ce0fea5e327cca53b72a3416edef7d3b'
